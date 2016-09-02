27f8e7b6d52fbc7e3e00a77774bfabc9948cb026
Mike Walters <mike@flomp.net>
2016-09-02 15:46:20 +0100

    Take target device name as argument

--- midiproxy.py
+++ midiproxy.py
@@ -1,17 +1,24 @@
+import argparse
 import mido
-
 mido.set_backend('mido.backends.rtmidi')
 
-proxy_input = mido.open_input('Proxy Input', virtual=True)
-proxy_output = mido.open_output('Proxy Output', virtual=True)
-target_input = mido.open_input(u'nanoKONTROL2 36:0')
-target_output = mido.open_output(u'nanoKONTROL2 36:0')
+def proxy(device_name):
+    proxy_input = mido.open_input('Proxy Input', virtual=True)
+    proxy_output = mido.open_output('Proxy Output', virtual=True)
+    target_input = mido.open_input(device_name)
+    target_output = mido.open_output(device_name)
 
-while True:
-    for txmsg in proxy_input.iter_pending():
-        target_output.send(txmsg)
-        print('=> ' + str(txmsg))
+    while True:
+        for txmsg in proxy_input.iter_pending():
+            target_output.send(txmsg)
+            print('=> ' + str(txmsg))
 
-    for rxmsg in target_input.iter_pending():
-        proxy_output.send(rxmsg)
-        print('<= ' + str(rxmsg))
+        for rxmsg in target_input.iter_pending():
+            proxy_output.send(rxmsg)
+            print('<= ' + str(rxmsg))
+
+parser = argparse.ArgumentParser(description='Proxy MIDI connection')
+parser.add_argument('target')
+
+args = parser.parse_args()
+proxy(args.target)
